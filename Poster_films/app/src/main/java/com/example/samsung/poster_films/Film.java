8bdb2e5d604cf323eaf7076e25d27b927fbcd699
Сахиб Гаджиев <Altair96.96@mail.ru>
2015-11-25 18:55:17 +0300

    Создания класа Film

---
+++ Poster_films/app/src/main/java/com/example/samsung/poster_films/Film.java
@@ -0,0 +1,61 @@
+package com.example.samsung.poster_films;
+
+import android.nfc.Tag;
+import android.util.Log;
+
+/**
+ * Created by Samsung on 24.11.2015.
+ */
+public class Film {
+    private String Url;
+    private String Name;
+    private String Text;
+    private String Premiere;
+
+    Film()
+    {
+
+        Name=new String();
+        Text=new String();
+        Premiere=new String();
+
+     Log.d("Create object", "Created!!!");
+    };
+
+
+
+
+    String getName()
+    {
+        return Name;
+    }
+
+    String getText()
+    {
+        return Text;
+
+    }
+    String getPremiere()
+    {
+        return Premiere;
+    }
+
+
+
+    void setName(String s)
+    {
+        Name=s;
+    }
+
+    void setText(String s)
+    {
+        Text=s;
+    }
+    void setPremiere(String s)
+    {
+        Premiere=s;
+    }
+
+
+
+}
